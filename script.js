baee7825bf53681855e4940081065bd2bf6651aa
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-10-14 12:19:27 -0400

    added event listener to select

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let form=document.querySelector("#form");
 let select=document.querySelector("#movie-selection");
+let section=document.querySelector("#display-info");
 fetch("https://ghibliapi.herokuapp.com/films")
    .then(data=> data.json())
       .then(obj=> {
@@ -9,4 +10,22 @@ fetch("https://ghibliapi.herokuapp.com/films")
               option.textContent= el.title;
               select.append(option);
           }
+          select.addEventListener("change", e=>{
+              for(let el of obj){
+                  if(e.target.value.toLowerCase()===el.title.toLowerCase()){
+                    section.innerHTML=`
+                    <h3>${el.title}</h3>
+                    <br>
+                    <p>${el.release_date}</p>
+                    <br>
+                    <p>${el.description}</p>`;
+
+                  }
+                //   break;
+              }
+
+                
+        
+        })
       });
+
